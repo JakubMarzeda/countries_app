205a0b697fa46e514dff1382662346227a329a32
JakubMarzeda <jakub.marzeda11@gmail.com>
2025-06-20 11:16:31 +0200

    feat: Changed model and added function to deserialize data from API to object

--- countries/lib/models/country.dart
+++ countries/lib/models/country.dart
@@ -1,8 +1,25 @@
 class Country {
-  String name;
-  String flagUrl;
-  String region;
-  List<String> capital; // Niektóre państwa mogą mieć więcej niż jedną stolicę np. RPA
-  int population;
-  Country(this.name, this.flagUrl, this.region, this.capital, this.population);
+  final String name;
+  final String flagUrl;
+  final String region;
+  final List<String> capital;
+  final int population;
+
+  Country({
+    required this.name,
+    required this.flagUrl,
+    required this.region,
+    required this.capital,
+    required this.population,
+  });
+}
+
+Country countryFromJson(Map<String, dynamic> json) {
+  return Country(
+    name: json["name"]["common"] ?? "Unknown",
+    flagUrl: json["flags"]["png"] ?? "",
+    region: json["region"] ?? "Unknown",
+    capital: List<String>.from(json["capital"] ?? []),
+    population: json["population"] ?? 0,
+  );
 }
